64221cbca14fdbc76ff490e653b28b52ffaf712e
Abdullah Abaga <112831592+essoviusss@users.noreply.github.com>
2023-06-24 19:57:54 +0800

    Dashboard Update

--- src/pages/AdmDashboard.js
+++ src/pages/AdmDashboard.js
@@ -10,21 +10,59 @@ import axios from "axios";
 import DonutChart from "react-donut-chart";
 import DirectionsCarRoundedIcon from '@mui/icons-material/DirectionsCarRounded';
 import CircleRoundedIcon from '@mui/icons-material/CircleRounded';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+import { Bar } from "react-chartjs-2";
 
 export default function AdmDashboard(){
     const [data, setData] = useState([]);
     const navigate = useNavigate();
     const isLoggedIn = useAuth();
     const [currentMonth, setCurrentMonth] = useState('');
+    const [totalPerMonthReq, setTotalPerMonthReq] = useState([]);
+    const [accomplishedReq, setAccomplishedReq] = useState([]);
+    const [cancelledReq, setCancelledReq] = useState([]);
 
     //read status
     const [pendingCount, setPendingCount] = useState(0);
     const [forApprovalCount, setForApprovalCount] = useState(0);
     const [approvedCount, setApprovedCount] = useState(0);
     const [accomplishedCount, setAccomplishedCount] = useState(0);
-    const [onTravelCount, setOnTravelCount] = useState(0);
-    const [onPMSCount, setOnPMSCount] = useState(0);
-    const [totalRequestCount, setTotalRequestCount] = useState(0);
+    const [rejectedCount, setRejectedCount] = useState(0);
+    const [cancelledCount, setCancelledCount] = useState(0);
+
+    const [vehicleOnPMS, setVehicleOnPMS] = useState(0);
+    const [driverOnTravel, setDriverOnTravel] = useState(0);
+    const [vehicleOnTravel, setVehicleOnTravel] = useState(0);
+
+    const totalCount = pendingCount + forApprovalCount + approvedCount + accomplishedCount + cancelledCount + rejectedCount;
+
+    const penPercent = (pendingCount / totalCount) * 100;
+    const forPercent = (forApprovalCount / totalCount) * 100;
+    const appPercent = (approvedCount / totalCount) * 100;
+    const accPercent = (accomplishedCount / totalCount) * 100;
+    const canPercent = (cancelledCount / totalCount) * 100;
+    const rejPercent = (rejectedCount / totalCount) * 100;
+
+    // Ensure that the sum of all percentages is 100%
+    const sumPercentages = penPercent + forPercent + appPercent + accPercent + canPercent + rejPercent;
+    const adjustmentFactor = 100 / sumPercentages;
+
+    const adjustedPenPercent = penPercent * adjustmentFactor;
+    const adjustedForPercent = forPercent * adjustmentFactor;
+    const adjustedAppPercent = appPercent * adjustmentFactor;
+    const adjustedAccPercent = accPercent * adjustmentFactor;
+    const adjustedCanPercent = canPercent * adjustmentFactor;
+    const adjustedRejPercent = rejPercent * adjustmentFactor;
+
+
 
     useEffect(() => {
         const token = localStorage.getItem("token");
@@ -60,10 +98,10 @@ export default function AdmDashboard(){
                 accomplishedData.append("status", "Accomplished");
 
                 let onTravelData = new FormData();
-                onTravelData.append("status", "On-Travel");
+                onTravelData.append("status", "Cancelled");
 
                 let onPMSData = new FormData();
-                onPMSData.append("status", "On-PMS");
+                onPMSData.append("status", "Rejected");
 
 
                 const response1 = await axios.post(url, pendingData);
@@ -79,10 +117,10 @@ export default function AdmDashboard(){
                 setAccomplishedCount(response4.data.totalcount);
 
                 const response5 = await axios.post(url, onTravelData);
-                setOnTravelCount(response5.data.totalcount);
+                setCancelledCount(response5.data.totalcount);
                 
                 const response6 = await axios.post(url, onPMSData);
-                setOnPMSCount(response6.data.totalcount);
+                setRejectedCount(response6.data.totalcount);
 
             }catch(e){
                 alert(e);
@@ -94,8 +132,8 @@ export default function AdmDashboard(){
         forApprovalCount, 
         approvedCount, 
         accomplishedCount, 
-        onPMSCount, 
-        onTravelCount
+        cancelledCount, 
+        rejectedCount
     ])
 
     useEffect(() => {
@@ -105,37 +143,141 @@ export default function AdmDashboard(){
       }, []);
 
       useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const url = `http://localhost/vreserv_api/accomplished_request.php`;
-            const userId = localStorage.getItem("userId");
-            let fData = new FormData();
-            fData.append("user_id", userId);
-    
-            const response = await axios.post(url, fData);
-            const { data } = response.data;
+        
             setData([
               {
-                value: data.accomplished_percentage,
+                value: penPercent,
+                label: "Pending",
+              },
+              {
+                value: forPercent,
+                label: "For Approval",
+              },
+              {
+                value: appPercent,
+                label: "Approved",
+              },
+              {
+                value: accPercent,
                 label: "Accomplished",
               },
               {
-                value: data.not_accomplished_percentage,
-                label: "Not Accomplished",
+                value: canPercent,
+                label: "Cancelled",
+              },
+              {
+                value: rejPercent,
+                label: "Rejected",
               },
             ]);
-          } catch (e) {
-            alert(e);
-          }
-        };
-    
-        fetchData();
-      }, []);
+       
+      }, [pendingCount, 
+        forApprovalCount, 
+        approvedCount, 
+        accomplishedCount, 
+        cancelledCount, 
+        rejectedCount
+    ]);
 
+      useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const url = `${BASE_URL}/permonth_request.php`;
+                
+                let fData = new FormData();
+                fData.append('status', '');
+
+                let fData1 = new FormData();
+                fData1.append('status', 'Accomplished');
+
+                let fData2 = new FormData();
+                fData2.append('status', 'Cancelled');
+
+                const response = await axios.post(url, fData);
+                const response1 = await axios.post(url, fData1);
+                const response2 = await axios.post(url, fData2);
+
+
+                setTotalPerMonthReq(response.data.total);
+                setAccomplishedReq(response1.data.request);
+                setCancelledReq(response2.data.request);
+
+            }catch(e){
+                alert(e);
+            }
+        }
+        fetchData();
+      },[])
+
+      const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October','November', 'December'];
+      
+      ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        BarElement,
+        Title,
+        Tooltip,
+        Legend
+      );
+
+    const data1 = {
+        labels,
+        datasets: [
+            {
+                label: 'Total Request',
+                data: totalPerMonthReq,
+                
+                backgroundColor: 'blue',
+            },
+            {
+                label: 'Accomplished Request',
+                data: accomplishedReq,
+                backgroundColor: 'red',
+            },
+            {
+                label: 'Cancelled Request',
+                data: cancelledReq,
+                backgroundColor: 'green',
+            },
+        ],
+    };
+
+    const options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top' ,
+          },
+          title: {
+            display: true,
+            text: `Current Month ${currentMonth}`,
+          },
+        },
+      };
+
+      useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const url = `${BASE_URL}/driver_vehicle_status.php`;
+
+                const response = await axios.get(url);
+                setVehicleOnPMS(response.data.vehicle_count);
+                setDriverOnTravel(response.data.driver_count);
+                setVehicleOnTravel(response.data.vehicle_count1);
+
+
+            }catch(e){
+                alert(e);
+            }
+        }   
+        fetchData();
+      },[vehicleOnPMS, vehicleOnTravel, driverOnTravel])
+      
     return(
         <div>
             <Header/>
             <div className="dash-container">
+                <h1>AdmDashboard</h1>
                 {/* top */}
                 <div className="upper">
                     <div className="top1Left">
@@ -146,19 +288,20 @@ export default function AdmDashboard(){
                         </div>
                         <div className="top1Below">
                             <div className="below-child">Accomplished {accomplishedCount}</div>
-                            <div className="below-child">On-Travel {onTravelCount}</div>
-                            <div className="below-child">On-PMS {onPMSCount}</div>
+                            <div className="below-child">Cancelled {cancelledCount}</div>
+                            <div className="below-child">Rejected {rejectedCount}</div>
                         </div>
                     </div>
                     <div className="top2Right">
-                        <p>Total Request {totalRequestCount}</p>
-                        
+                        <p>On-Travel Vehicle/s {vehicleOnTravel}</p>
+                        <p>On-Travel Driver/s {driverOnTravel}</p>
+                        <p>On-PMS Vehicle/s {vehicleOnPMS}</p>
                     </div>
                 </div>
                 {/* below */}
                 <div className="middle">
                     <div className="below1Left">
-                        <p>Current Month: {currentMonth}</p> 
+                        <Bar options={options} data={data1} />
                     </div>
                     <div className="below2Right">
                     <div className="donut-chart-container">
@@ -169,7 +312,7 @@ export default function AdmDashboard(){
                             strokeWidth={100}
                             className="custom-donut-chart"
                             legend={false}
-                            colors={['#025BAD', '#D9D9D9']}
+                            colors={['#025BAD', '#D9D9D9', '#FFC300', '#9C27B0', '#00BCD4', '#FF5722']}
                             hoverColors={['#025BAD', '#D9D9D9']}
                         />
                         <div className="donut-icon">
@@ -177,18 +320,42 @@ export default function AdmDashboard(){
                         </div>
                         </div>
                         <div className="donut-legends">
-                        <div className="donut-title">Scheduled Travels</div>
+                        <div className="donut-title">Status Percentage</div>
                         <div className="donut-accomplished">
                             <CircleRoundedIcon style={{ fontSize: 20, color: '#025BAD' }} /> &nbsp;
-                            <div className="percentage">25%</div> &nbsp;
-                            <div className="per-label">Accomplished</div>
-                            <div className="count-label">(31 Travels)</div>
+                            <div className="percentage">{adjustedPenPercent.toFixed(2)}%</div> &nbsp;
+                            <div className="per-label">Pending</div>
+                            <div className="count-label">({pendingCount} Request/s)</div>
                         </div>
                         <div className="donut-unaccomplished">
                         <CircleRoundedIcon style={{ fontSize: 20, color: '#D9D9D9' }} />  &nbsp;
-                            <div className="percentage">50%</div> &nbsp;
-                            <div className="per-label">Unccomplished</div>
-                            <div className="count-label">(15 Travels)</div>
+                            <div className="percentage">{adjustedForPercent.toFixed(2)}%</div> &nbsp;
+                            <div className="per-label">For Approval</div>
+                            <div className="count-label">({forApprovalCount} Request/s)</div>
+                        </div>
+                        <div className="donut-accomplished">
+                            <CircleRoundedIcon style={{ fontSize: 20, color: '#FFC300' }} /> &nbsp;
+                            <div className="percentage">{adjustedAppPercent.toFixed(2)}%</div> &nbsp;
+                            <div className="per-label">Approved</div>
+                            <div className="count-label">({approvedCount} Request/s)</div>
+                        </div>
+                        <div className="donut-unaccomplished">
+                        <CircleRoundedIcon style={{ fontSize: 20, color: '#9C27B0' }} />  &nbsp;
+                            <div className="percentage">{adjustedAccPercent.toFixed(2)}%</div> &nbsp;
+                            <div className="per-label">Accomplished</div>
+                            <div className="count-label">({accomplishedCount} Request/s)</div>
+                        </div>
+                        <div className="donut-accomplished">
+                            <CircleRoundedIcon style={{ fontSize: 20, color: '#00BCD4' }} /> &nbsp;
+                            <div className="percentage">{adjustedCanPercent.toFixed(2)}%</div> &nbsp;
+                            <div className="per-label">Cancelled</div>
+                            <div className="count-label">({cancelledCount} Request/s)</div>
+                        </div>
+                        <div className="donut-unaccomplished">
+                        <CircleRoundedIcon style={{ fontSize: 20, color: '#FF5722' }} />  &nbsp;
+                            <div className="percentage">{adjustedRejPercent.toFixed(2)}%</div> &nbsp;
+                            <div className="per-label">Rejected</div>
+                            <div className="count-label">({rejectedCount} Request/s)</div>
                         </div>
                     </div>
                     </div>
